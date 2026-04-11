bda221c780ca0263e001eca5c606f4069c6c095b
yarnbytes <yl1247916822@gmail.com>
2026-04-11 20:44:32 +0800

    fix: register tauri_plugin_opener in backend to prevent application crashes when launching external URLs

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -217,6 +217,7 @@ fn build_tray(app: &tauri::App) -> tauri::Result<()> {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_clipboard_manager::init())
